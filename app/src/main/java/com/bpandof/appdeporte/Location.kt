a4fd4beaae1eccfb1540947ad009fcabea51d0e4
bernabep <bernabepando@gmail.com>
2022-11-17 19:16:42 +0100

    cargar recorrido

---
+++ app/src/main/java/com/bpandof/appdeporte/Location.kt
@@ -0,0 +1,3 @@
+package com.bpandof.appdeporte
+
+data class Location()
---
+++ app/src/main/java/com/bpandof/appdeporte/RunActivity.kt
@@ -0,0 +1,11 @@
+package com.bpandof.appdeporte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_run)
+    }
+}
